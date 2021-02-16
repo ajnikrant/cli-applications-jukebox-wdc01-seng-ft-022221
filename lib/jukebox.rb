38b9a4291b600a7e78c95b9e9ceec4db29bfe623
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-16 22:38:03 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,27 @@
 # Add your code here
+require 'pry'
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play
+
+end
+
+def list
+  songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+    binding.pry
+  end
+end
+
+def exit_jukebox
+end
+
+def run
+end
