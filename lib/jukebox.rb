27f4bee144a19dce8175ea5f33d7a2a08509692e
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-16 23:21:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,21 +11,27 @@ def help
 end
 
 def play(songs)
-  counter=0
-  user_input = gets.chomp
-  puts "Please enter a song name or number:"
-
-  while counter < songs.length
-    if user_input.to_i== counter+1 || user_input == songs[counter]
-      puts "Playing #{songs[counter]}"
-    else
-      puts "Invalid input, please try again"
-    end
-    # binding.pry
-
-    counter +=1
-  end
+  # counter=0
+  # user_input = gets.chomp
+  # puts "Please enter a song name or number:"
+  # 
+  # while counter < songs.length
+  #   if user_input.to_i== counter+1 || user_input == songs[counter]
+  #     puts "Playing #{songs[counter]}"
+  #   else
+  #     puts "Invalid input, please try again"
+  #   end
+  #   # binding.pry
+  # 
+  #   counter +=1
+  # end
 # binding.pry
+puts "Please enter a song name"	  #this method is slightly different!
+  song_to_play = gets.chomp	  #you should still ask the user for input and collect their song choice
+  if !my_songs.keys.include?(song_to_play)	  #this time, only allow user's to input a song name
+    puts "Invalid input, please try again"	  #check to see if the name they give is in fact a key of the my_songs hash
+  else	  #if it isn't, tell them their choice is invalid
+    puts "Now playing #{song_to_play}"
 end
 
 def list(songs)
