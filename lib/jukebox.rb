a0e53596ace4ef4585c239cd7a143cb509913827
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-16 23:49:27 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -43,7 +43,7 @@ def run(songs)
   while user_input
     puts "Please enter a command:"
     user_input=gets.strip
-    case input
+    case user_input
       when "list"
         list(songs)
       when "play"
